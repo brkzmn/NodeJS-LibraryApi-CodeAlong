b18878d9a9390ec3af16fe63124292d9f0fca707
Burak Özmen <brkzmn.wd@gmail.com>
2022-03-14 12:26:31 +0100

    app-done

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-//main server file
+"use strict";
 
 const express = require("express");
+const { type } = require("express/lib/response");
 const { v4: uuidv4 } = require("uuid");
 const app = express();
 
@@ -24,12 +25,48 @@ app.delete("/books/:id", function (req, res) {
   deleteBook(req, res);
 });
 
-function createBook(req, res) {
+function deleteBook(req, res) {
+  const bookToDelete = data.find((book) => book.id === req.params.id);
+  if (typeof bookToDelete == "undefined") {
+    res.status(404);
+    res.send("No such book");
+    return;
+  }
+  data.splice(data.indexOf(bookToDelete), 1);
+  res.send("ok");
+}
+
+function isInvalid(req) {
   if (
     typeof req.body == "undefined" ||
     typeof req.body.title == "undefined" ||
     typeof req.body.author == "undefined"
   ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateBook(req, res) {
+  if (isInvalid(req)) {
+    res.status(400);
+    res.send("invalid request");
+    return;
+  }
+  const bookToUpdate = data.find((book) => book.id === req.params.id);
+  if (typeof bookToUpdate == "undefined") {
+    res.status(404);
+    res.send("No such book");
+    return;
+  }
+  bookToUpdate.title = req.body.title;
+  bookToUpdate.author = req.body.author;
+  res.send("ok");
+}
+
+function createBook(req, res) {
+  if (isInvalid(req)) {
     res.status(400);
     res.send("invalid request");
     return;
@@ -41,6 +78,7 @@ function createBook(req, res) {
     author: req.body.author,
   };
   data.push(newBook);
+  res.status(201);
   res.send(id);
 }
 
